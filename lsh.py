1d4886e875a246a3ed64ffac041629d0b74b3dd5
Chris Kamphuis <mail@chriskamphuis.com>
2016-08-21 00:20:20 +0200

    Update shingle function
    Now the shingle function also accepts words as tokens, it splits on tabs,
    spaces and newlines.

--- lsh.py
+++ lsh.py
@@ -19,13 +19,31 @@ class lsh:
                             buf[i] = buf[i+1]
                         buf[w-1] = c
                         if buf[0] is not '':
-                            shingle = ""
+                            shingle = ''
                             for t in buf:
                                 shingle += t
                             shingle_list.append(shingle)
-
             elif token is 'word':
-                print(token)
+                word_buf = ''
+                while True:
+                    c = doc.read(1)
+                    if not c:
+                        break
+                    elif c is not '\n' and c is not ' ' and c is not '\t':
+                        word_buf += c
+                    elif word_buf is not '':
+                        for i in range(w-1):
+                            buf[i] = buf[i+1]
+                        buf[w-1] = word_buf
+                        if buf[0] is not '':
+                            shingle = ''
+                            for t in buf:
+                                shingle += t
+                                shingle += ' '
+                            shingle = shingle[0:-1]
+                            shingle_list.append(shingle)
+                        word_buf = ''
+
             else:
                 print('invalid token')
                 exit(0)
