c42624da90c4ab570346dda073532c4c50ae9ddb
Chris Kamphuis <mail@chriskamphuis.com>
2016-08-21 19:05:51 +0200

    Update shingle function
    Made it such that the shingle function does not return bags of shingles per
    document, but instantly puts in a mutli dimensional map. This way it will be
    easier to make minhashes after shingling the documents.

--- lsh.py
+++ lsh.py
@@ -4,7 +4,9 @@ import numpy as np  # Numpy is used to have more control over the datatypes.
 class lsh:
 
     def __init__(self):
-        print("")
+        self.hdhm = {}  # 2d hash map for saving doc shingle pairs.
+        self.docs = set()  # list for saving which docs exists.
+        self.shingles = set()  # list for saving which shingle hashes exists.
 
     '''
     Creates a shingle set of a text document.
@@ -13,15 +15,15 @@ class lsh:
     troublesome if you use special characters.
     '''
     def shingle(self, document, w=5, token='char'):
-        shingle_list = np.array([])
         buf = ['' for i in range(w)]
         with open(document, 'r') as doc:
+            self.docs |= {document}
             if token is 'char':
                 while True:
                     c = doc.read(1)
                     if not c:
                         break
-                    elif c is '\n' or c is ' ' or c is '\t':
+                    elif c is '\n':
                         continue
                     else:
                         for i in range(w-1):
@@ -33,7 +35,8 @@ class lsh:
                                 shingle += t
                             shinglehash = hash(shingle) % (2**32)
                             shingle32 = np.uint32(shinglehash)
-                            shingle_list = np.append(shingle_list, shingle32)
+                            self.shingles |= {shingle32}
+                            self.hdhm[(document, shingle32)] = True
             elif token is 'word':
                 word_buf = ''
                 while True:
@@ -54,13 +57,13 @@ class lsh:
                             shingle = shingle[0:-1]
                             shinglehash = hash(shingle) % (2**32)
                             shingle32 = np.uint32(shinglehash)
-                            shingle_list = np.append(shingle_list, shingle32)
+                            self.shingles |= {shingle32}
+                            self.hdhm[(document, shingle32)] = True
                         word_buf = ''
 
             else:
                 print('invalid token')
                 exit(0)
-        return np.unique(shingle_list)
 
     '''
     Jaccard function for determining similarity between sets.
@@ -69,8 +72,3 @@ class lsh:
         intersect = np.intersect1d(set1, set2)
         union = np.union1d(set1, set2)
         return float(len(intersect))/float(len(union))
-
-a = lsh()
-b = a.shingle("test.txt")
-c = a.shingle("test2.txt")
-print(a.jaccard(b, c))
